583f46b2bea3a3ba361f6708b7dccdc4e2cd189e
kjraver <141275416+kjraver@users.noreply.github.com>
2023-10-09 10:52:08 -0500

    dataframe updates

--- Dahlke Data Request.R
+++ Dahlke Data Request.R
@@ -4,7 +4,7 @@ library(googlesheets4)
 library(ggridges)
 
 IA<-read_sheet('https://docs.google.com/spreadsheets/d/1Dazv10GXyhBLlo3Ql-tcvQBdP_uoW73VNfc-Dq0TxkY/edit?usp=sharing', ".")
-IAmin<- read_sheet('https://docs.google.com/spreadsheets/d/1Dazv10GXyhBLlo3Ql-tcvQBdP_uoW73VNfc-Dq0TxkY/edit?usp=sharing', "Sheet1")
+IAmin<- read_sheet('https://docs.google.com/spreadsheets/d/1Dazv10GXyhBLlo3Ql-tcvQBdP_uoW73VNfc-Dq0TxkY/edit?usp=sharing', "Table_Statistics Dump")
 
 
 ggplot(data=IA, aes(x=NRC2001_Lignin_tdn, y=fdProductCode))+
@@ -24,7 +24,7 @@ ggplot(data=IA, aes(x=Beef_Ton, y=fdProductCode))+
 
 
 
-ggplot(data=IAmin, aes(x=fdsnMg, y=MaterialType))+
+ggplot(data=IAmin, aes(x=fdsnMg, y=FeedType))+
   stat_density_ridges(quantile_lines = TRUE, quantiles = 3)
 
 ggplot(data=IAmin, aes(x=fdsngCa, y=MaterialType))+
